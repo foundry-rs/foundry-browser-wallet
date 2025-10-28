c6ffa1e3d49579e71f44b451cf6afccdf9c9ef1f
zerosnacks <zerosnacks@protonmail.com>
2025-10-28 12:39:35 +0100

    define default port, add injection point for token

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,23 @@ import react from "@vitejs/plugin-react-swc";
 import mkcert from "vite-plugin-mkcert";
 
 export default defineConfig({
-  plugins: [mkcert(), react()],
+  plugins: [
+    mkcert(),
+    react(),
+    {
+      name: "add-session-token-banner",
+      generateBundle(_, bundle) {
+        for (const file of Object.values(bundle)) {
+          if (file.type === "chunk" && file.fileName === "main.js") {
+            file.code = "/** SESSION_TOKEN */\n" + file.code;
+          }
+        }
+      },
+    },
+  ],
+  server: {
+    port: 9545,
+  },
   build: {
     outDir: "dist",
     assetsDir: ".",
